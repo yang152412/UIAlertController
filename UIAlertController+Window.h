6812f40244c25be462ab8f1f1491af4c4013ad24
yang152412 <yang152412@163.com>
2017-02-07 20:38:18 +0800

    add recommend files

---
+++ UIAlertController+Window.h
@@ -0,0 +1,16 @@
+//
+//  UIAlertController+Window.h
+//  neighborhood
+//
+//  Created by Yang on 2017/2/6.
+//  Copyright © 2017年 iYaYa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertController (Window)
+
+- (void)show;
+- (void)show:(BOOL)animated;
+
+@end
